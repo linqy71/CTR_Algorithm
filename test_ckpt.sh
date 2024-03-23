97a40f3e7f53ed6edc93353230642e694181e775
linqy71 <2575769386@qq.com>
2024-03-23 18:24:39 +0800

    add script

---
+++ test_ckpt.sh
@@ -0,0 +1,63 @@
+lsecp=1
+diff=1
+incre=1
+rocksdb=1
+
+ckpt_dir="/mnt/ssd/deepfm"
+
+dataset_path="/mnt/ssd/dataset/kaggle/train_sample.txt"
+
+check_freq=10
+num_batches=1000
+
+if [ $lsecp=1 ]; then
+
+  python run_deepfm_mp.py --num-batches=$num_batches \
+    --ckpt-method="lsecp" \
+    --dataset-path=$dataset_path \
+    --ckpt-freq=$check_freq \
+    --ckpt-dir=$ckpt_dir \
+    --lsecp-eperc=0.01 \
+    --lsecp-clen=10 \
+    --perf-out-path="./lsecp.json"
+
+fi
+
+if [ $diff=1 ]; then
+
+  python run_deepfm_mp.py --num-batches=$num_batches \
+    --ckpt-method="diff" \
+    --dataset-path=$dataset_path \
+    --ckpt-freq=$check_freq \
+    --ckpt-dir=$ckpt_dir \
+    --lsecp-eperc=0.01 \
+    --lsecp-clen=10 \
+    --perf-out-path="./diff.json"
+
+fi
+
+if [ $incre=1 ]; then
+
+  python run_deepfm_mp.py --num-batches=$num_batches \
+    --ckpt-method="incre" \
+    --dataset-path=$dataset_path \
+    --ckpt-freq=$check_freq \
+    --ckpt-dir=$ckpt_dir \
+    --lsecp-eperc=0.01 \
+    --lsecp-clen=10 \
+    --perf-out-path="./incre.json"
+
+fi
+
+if [ $rocksdb=1 ]; then
+
+  python run_deepfm_mp.py --num-batches=$num_batches \
+    --ckpt-method="rocksdb" \
+    --dataset-path=$dataset_path \
+    --ckpt-freq=$check_freq \
+    --ckpt-dir=$ckpt_dir \
+    --lsecp-eperc=0.01 \
+    --lsecp-clen=10 \
+    --perf-out-path="./rocksdb.json"
+
+fi
